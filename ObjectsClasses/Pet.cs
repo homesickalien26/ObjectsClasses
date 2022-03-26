1161e6295f7e8df5a7636c75d4699be5995fc2e7
Homesickalien26 <athomas2058@gmail.com>
2022-03-26 18:05:24 -0400

    Add project files.

---
+++ ObjectsClasses/Pet.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ObjectsClasses
+{
+    class Pet
+    {
+        public int Age;
+        public string Name;
+        public string Breed;
+        public string Gender;
+
+
+    }
+}
+
---
+++ ObjectsClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ObjectsClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many pets do you have?")
+                
+
+            Pet petInfo = new Pet();
+            Console.WriteLine("What is your pets name?");
+            petInfo.Name = Console.ReadLine();
+            Console.WriteLine("What is your pets breed");
+            petInfo.Breed = Console.ReadLine();
+            Console.WriteLine("What is your dogs age?");
+            petInfo.Age = int.Parse(Console.ReadLine());
+           
+
+        }
+
+    }
+}
