115920f93234c371bf55868bd8c9530a518f2654
homesickalien26 <78085906+homesickalien26@users.noreply.github.com>
2022-03-26 18:35:13 -0400

    Update Program.cs

--- ObjectsClasses/Program.cs
+++ ObjectsClasses/Program.cs
@@ -8,8 +8,10 @@
                 // array would go here
                 int arrayPetCount = new int(Console.ReadLine));
                 
-                for (int i = 0 ; i < arrayPetCount ; i++)
-                // this is where id seperate the array so the user can imput data for each pet. while creating a loop depending on how many pets the user has
+              
+                // this is where id seperate the array so the user can imput data for each pet. while creating a loop depending on how many pets the user 
+                    // Im stuck on the idea that a user may have 2 dogs...lets next get the first dogs name..then for that dog lets get its age and its breed.
+                    // lets then loop that so the user can get the 2nd dogs name. etc 
             Pet petInfo = new Pet();
             Console.WriteLine("What is your pets name?");
             petInfo.Name = Console.ReadLine();
