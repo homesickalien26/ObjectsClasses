64026129a4ff7d1a530615f156c95992902b6bf0
homesickalien26 <78085906+homesickalien26@users.noreply.github.com>
2022-03-26 18:24:34 -0400

    Update Program.cs

--- ObjectsClasses/Program.cs
+++ ObjectsClasses/Program.cs
@@ -5,8 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("How many pets do you have?")
+                // array would go here
                 
-
+                
+                
+                // this is where id seperate the array so the user can imput data for each pet. while creating a loop depending on how many pets the user has
             Pet petInfo = new Pet();
             Console.WriteLine("What is your pets name?");
             petInfo.Name = Console.ReadLine();
